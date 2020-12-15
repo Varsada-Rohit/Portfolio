86704533937333ea9be68534cb7e70c14e6a1ad4
Rohit <varsadarohit@gmail.com>
2020-12-15 10:08:55 +0530

    first

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<nav className="navbar navbar-expand-lg navbar-light bg-light ">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Nexart</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse"  id="navbarSupportedContent">
+      <ul className="nav navbar-nav ml-auto w-100  justify-content-end">
+        <li className="nav-item">
+          <a className="nav-link active " aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+     
+      </ul>
+    </div>
+  </div>
+</nav>
     </div>
   );
 }
